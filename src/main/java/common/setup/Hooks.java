3f237d70a2617a38948b7094bf98f0ad2fcd2e43
Zoltan Kis <hiqatech@gmail.com>
2022-08-01 23:24:43 +0200

    Update Hooks.java

--- src/main/java/common/setup/Hooks.java
+++ src/main/java/common/setup/Hooks.java
@@ -1,8 +1,8 @@
 package common.setup;
 
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.junit.Assert;
 
 import java.time.LocalDateTime;
