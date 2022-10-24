54b15ae97852b76784605a6d883fd2b7bebc1e9c
Zoltan Kis <hiqatech@gmail.com>
2022-10-24 11:22:29 +0200

    Update TestRunner.java
    update

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,35 +1,21 @@
-import com.cucumber.listener.Reporter;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
-import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
         (features = {"src/test/resources/features"},
-        tags = { "@Smoke", "@Regression" },
-        glue = {"classpath:common/cucumber","classpath:common.setup","classpath:products"},
-                plugin = {
-                        "html:target/cucumber-reports/",
-                        "junit:target/cucumber-reports/report.xml",
-                        "json:target/cucumber-reports/report.json",
-                        "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"
+                glue = {"classpath:com/stepdefs","classpath:com.setup","classpath:product"},
+                plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
                 }
         )
 
-public class TestRunner {
+public class RunCukesTest {
 
     @AfterClass
     public static void report()
     {
-        Reporter.loadXMLConfig(new File("src/main/resources/report/extent-config.xml"));
-        Reporter.setSystemInfo("User Name", "HiQATech");
-        Reporter.setSystemInfo("Application Name", "Test App ");
-        Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
-        Reporter.setSystemInfo("Environment", "QA");
-        Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
     }
 
 }
-
